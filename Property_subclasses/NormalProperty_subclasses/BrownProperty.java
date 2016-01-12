8f5a5dacc5fd3af7ba61967b662074f90292f281
Calvin Vuong <calvin@boxer.local>
2016-01-12 18:28:32 -0500

    UML updated, started work on BrownProperty

---
+++ Property_subclasses/NormalProperty_subclasses/BrownProperty.java
@@ -0,0 +1,9 @@
+public class BrownProperty extends NormalProperty{
+   
+    // pre-set rent prices, index corresponds w/ # of houses
+    protected final int[] RENT1 = {2, 10, 30, 90, 160, 250}; //mediterranean
+    protected final int[] RENT2 = {4, 20, 60, 180, 320, 450}; //baltic
+
+    
+
+    
