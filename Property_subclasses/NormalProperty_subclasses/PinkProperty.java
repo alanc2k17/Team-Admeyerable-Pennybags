30e91071beedf8f839ee0598aee3563c9de76ea0
alanc2k17 <ychen15@stuy.edu>
2016-01-12 20:48:26 -0500

    Update PinkProperty.java

---
+++ Property_subclasses/NormalProperty_subclasses/PinkProperty.java
@@ -0,0 +1,34 @@
+public class PinkProperty extends NormalProperty{
+   
+    //default constructor
+    public PinkProperty(){
+    	super();
+    	setInfo();
+    }
+
+    //overloaded constructor
+    public PinkProperty(String name, String initials, String coordinate, boolean expensive){
+    	super(name, initials, coordinate, expensive);
+    	setInfo();
+    }
+
+    //set rent prices, buy prices, house costs, and mortgage values
+    public void setInfo(){
+	// set rent prices, index corresponds w/ # of houses
+    	_rent1 = {10, 50, 150, 450, 625, 750}; //virginia ave
+    	_rent2 = {10, 50, 150, 450, 625, 750}; //states ave
+	    _rent3 = {12, 60, 180, 500, 700, 900}; //st. charles place
+	    
+	// set buy prices
+    	_buyPrice1 = 140; 
+    	_buyPrice2 = 140; 
+	    _buyPrice3 = 160; 
+	    
+	//set house cost and mortgage value
+    	_houseCost = 100;
+    	_mortgageValue1 = (int) (.5 * _buyPrice1);
+    	_mortgageValue2 = (int) (.5 * _buyPrice2);
+    	_mortgageValue3 = (int) (.5 * _buyPrice3);
+
+    }
+}
