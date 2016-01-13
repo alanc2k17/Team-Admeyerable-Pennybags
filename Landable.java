8ddf546a692e31ff186631d836686c6541c49229
alanc2k17 <ychen15@stuy.edu>
2016-01-13 11:42:51 -0500

    Update Landable.java

--- Landable.java
+++ Landable.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public abstract class Landable {
   protected int _coordinate;
   protected String _name;
@@ -5,20 +7,31 @@ public abstract class Landable {
   protected ArrayList<Player> _playerOnSquare
   
   public Landable() {
+    _coordinate = 0;
+    _name = "board square";
+    _initials = "xD";
+    _playerOnSquare = new ArrayList<Player>();
   }
   
-  public Landable (String name, String initials, String coordinate) {
+  public Landable (String name, String initials, int coordinate) {
+    this();
+    _coordinate = coordinate;
+    _name = name;
+    _initials = initials;
   }
   
   public abstract String toString() {
   }
   
   public int getCoordinate() {
+    return _coordinate;
   }
   
   public void setPlayerOnSquare(Player p) {
+    _playerOnSquare.add(p);
   }
   
   public void removePlayerOnSquare(Player p) {
+    _playerOnSquare.remove(p);
   }
 } //end class Landable
