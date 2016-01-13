db64f69d37d33441adb059f9f2312bb2a0d9ccf8
alanc2k17 <ychen15@stuy.edu>
2016-01-13 11:21:32 -0500

    Update Property.java

---
+++ Property.java
@@ -0,0 +1,29 @@
+public abstract class Property extends Landable {
+  protected Player _owner;
+  protected ArrayList<Player> _playersOnProperty;
+  protected ArrayList<Property> _groupMembers;
+  protected boolean _mortgage;
+  
+  public Property() {
+  }
+  
+  public Property(String name, String initials, String coordiante) {
+  }
+  
+  public abstract int getRent() {
+  }
+  
+  public String toString() {
+  }
+  
+  public int getBuyPrice() {
+  }
+  
+  public Player getOwner() {
+  }
+  
+  public void addMembers(Property p) {
+  
+  
+  public int mortgage() {
+  }
