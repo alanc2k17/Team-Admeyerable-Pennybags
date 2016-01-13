5a587e4859cef3e4cb7ce26ec302765810c4ef65
Calvin Vuong <calvin@boxer.local>
2016-01-12 19:43:20 -0500

    BrownProperty and LightBlueProperty done; UML updated

--- Property_subclasses/NormalProperty_subclasses/BrownProperty.java
+++ Property_subclasses/NormalProperty_subclasses/BrownProperty.java
@@ -1,26 +1,32 @@
 public class BrownProperty extends NormalProperty{
    
-    // pre-set rent prices, index corresponds w/ # of houses
-    protected final int[] RENT1 = {2, 10, 30, 90, 160, 250}; //mediterranean
-    protected final int[] RENT2 = {4, 20, 60, 180, 320, 450}; //baltic
-
-    // pre-set buy prices
-    protected final int BUYPRICE1 = 60; //mediterranean
-    protected final int BUYPRICE2 = 60; //baltic
-    
-    //pre-set house cost and mortgage value
-    protected final int HOUSECOST = 50;
-    protected final int MORTGAGEVALUE1 = 30;
-    protected final int MORTGAGEVALUE2 = 30;
-
     //default constructor
     public BrownProperty(){
-	this();	
+	super();
+	setInfo();
     }
 
     //overloaded constructor
     public BrownProperty(String name, String initials, String coordinate, boolean expensive){
-	this(name, initials, coordinate, expensive);
+	super(name, initials, coordinate, expensive);
+	setInfo();
+    }
+
+    //set rent prices, buy prices, house costs, and mortgage values
+    public void setInfo(){
+	// set rent prices, index corresponds w/ # of houses
+	_rent1 = {6, 30, 90, 270, 400, 550}; //mediterranean
+	_rent2 = {8, 40, 100, 300, 450, 600}; //baltic
+	
+	// set buy prices
+	_buyPrice1 = 100; 
+	_buyPrice2 = 120; 
+	
+	//set house cost and mortgage value
+	_houseCost = 50;
+	_mortgageValue1 = (int) (.5 * _buyPrice1);
+	_mortgageValue2 = (int) (.5 * _buyPrice2);
+
     }
 }
 
---
+++ Property_subclasses/NormalProperty_subclasses/LightBlueProperty.java
@@ -0,0 +1,33 @@
+public class LightBlueProperty extends NormalProperty{
+   
+    //default constructor
+    public BrownProperty(){
+	super();
+	setInfo();
+    }
+
+    //overloaded constructor
+    public BrownProperty(String name, String initials, String coordinate, boolean expensive){
+	super(name, initials, coordinate, expensive);
+	setInfo();
+    }
+
+    //set rent prices, buy prices, house costs, and mortgage values
+    public void setInfo(){
+	// set rent prices, index corresponds w/ # of houses
+	_rent1 = {6, 30, 90, 270, 400, 550}; //oriental and vermont
+	_rent2 = {8, 40, 100, 300, 450, 600}; //connecticut
+	
+	// set buy prices
+	_buyPrice1 = 100; 
+	_buyPrice2 = 120; 
+	
+	//set house cost and mortgage value
+	_houseCost = 50;
+	_mortgageValue1 = (int) (.5 * _buyPrice1);
+	_mortgageValue2 = (int) (.5 * _buyPrice2);
+
+    }
+}
+
+    
