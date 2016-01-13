5049c998603c6578fc03d6bb34c0d2166ab9de1d
Calvin Vuong <calvin@boxer.local>
2016-01-13 18:56:44 -0500

    Landable commented; Tax mostly completed, except for faulty toString(), UML updated

--- Landable.java
+++ Landable.java
@@ -4,34 +4,40 @@ public abstract class Landable {
     protected int _coordinate;
     protected String _name;
     protected String _initials;
-    protected ArrayList<Player> _playerOnSquare;
+    // last Player in list will be player most recently landed
+    protected ArrayList<Player> _playersOnSquare;
   
-      
+    // default constructor; written even though class is abstract so it can be inherited
     public Landable() {
 	_coordinate = 0;
 	_name = "board square";
 	_initials = "xD";
-	_playerOnSquare = new ArrayList<Player>();
+	_playersOnSquare = new ArrayList<Player>();
     }
   
+    // overloaded constructor; written even though class is abstract so it can be inherited
     public Landable (String name, String initials, int coordinate) {
-	this();
+	this(); //call default constr. to instanitate _playersOnSquare
 	_coordinate = coordinate;
 	_name = name;
 	_initials = initials;
     }
   
+    // different subclasses will display themselves differently
     public abstract String toString();
   
     public int getCoordinate() {
 	return _coordinate;
     }
-  
+    
+    // called after a Player lands on this square
     public void setPlayerOnSquare(Player p) {
 	_playerOnSquare.add(p);
     }
   
+    // called after a Player moves after it is on this square
     public void removePlayerOnSquare(Player p) {
 	_playerOnSquare.remove(p);
     }
+
 } //end class Landable
--- Tax.java
+++ Tax.java
@@ -1,16 +1,41 @@
 public class Tax {
     protected int _payAmount;
-  
+    
+    // default constructor
     public Tax() {
+	_payAmount = 100;
     }
   
+    // overrided constructor
+    // sets _payAmount to parameter value
     public Tax(int payAmount) {
+	_payAmount = payAmount;
     }
   
+    // must be implemented
     public String toString() {
+
+	String line1 = "------\n";
+	String line2 = "| TAX|\n";
+	String line3 = "|$" + _payAmount + "|\n";
+
+	
+	//line four holds symbols of all players on property
+	String line4 = "|";
+	//print out all Players on this square onto line4
+	for (int i = 0; i < _playersOnSquare.size(); i++){ 
+	    line4 += _playersOnSquare.get(i);
+	}
+	line4 += "|\n";
+		
+	String line5 = "------";
+
+	return line1 + line2 + line3 + line4 + line5;
     }
   
-    public int getAmount() {
+    //accessor; returns tax amount
+    public int getTax() {
+	return _payAmount;
     }
-  
+
 } //end Tax
