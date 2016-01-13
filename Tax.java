72c857b4d62988d89799d7ce19775d86ef5a7ebc
alanc2k17 <ychen15@stuy.edu>
2016-01-13 11:23:16 -0500

    Create Tax.java

---
+++ Tax.java
@@ -0,0 +1,16 @@
+public class Tax {
+  protected int _payAmount;
+  
+  public Tax() {
+  }
+  
+  public Tax(int payAmount) {
+  }
+  
+  public String toString() {
+  }
+  
+  public int getAmount() {
+  }
+  
+} //end Tax
