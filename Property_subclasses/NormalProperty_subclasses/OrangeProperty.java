9f810234836aee82ea4485b6765748146debe945
alanc2k17 <ychen15@stuy.edu>
2016-01-12 21:02:23 -0500

    Update OrangeProperty.java

--- Property_subclasses/NormalProperty_subclasses/OrangeProperty.java
+++ Property_subclasses/NormalProperty_subclasses/OrangeProperty.java
@@ -15,20 +15,17 @@ public class OrangeProperty extends NormalProperty{
     //set rent prices, buy prices, house costs, and mortgage values
     public void setInfo(){
 	// set rent prices, index corresponds w/ # of houses
-    	_rent1 = {14, 70, 200, 550, 700, 900}; //st. james place
-    	_rent2 = {14, 70, 200, 550, 700, 900}; //tennessee ave
-	    _rent3 = {16, 80, 220, 600, 800, 1000}; //ny ave
+    	_rent1 = {14, 70, 200, 550, 700, 900}; //st. james place | tennessee ave
+	_rent2 = {16, 80, 220, 600, 800, 1000}; //ny ave
 	    
 	// set buy prices
     	_buyPrice1 = 180; 
-    	_buyPrice2 = 180; 
-	    _buyPrice3 = 200; 
+    	_buyPrice2 = 200; 
 	    
 	//set house cost and mortgage value
     	_houseCost = 100;
     	_mortgageValue1 = (int) (.5 * _buyPrice1);
     	_mortgageValue2 = (int) (.5 * _buyPrice2);
-    	_mortgageValue3 = (int) (.5 * _buyPrice3);
 
     }
 }
