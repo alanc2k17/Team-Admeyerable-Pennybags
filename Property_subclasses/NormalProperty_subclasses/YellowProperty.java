c930f3069152ac05bb09af3901d8784c40faf363
alanc2k17 <ychen15@stuy.edu>
2016-01-12 21:03:46 -0500

    Update YellowProperty.java

--- Property_subclasses/NormalProperty_subclasses/YellowProperty.java
+++ Property_subclasses/NormalProperty_subclasses/YellowProperty.java
@@ -15,20 +15,17 @@ public class YellowProperty extends NormalProperty{
     //set rent prices, buy prices, house costs, and mortgage values
     public void setInfo(){
 	// set rent prices, index corresponds w/ # of houses
-    	_rent1 = {22, 110, 330, 800, 975, 1150}; //atlantic ave
-    	_rent2 = {22, 110, 330, 800, 975, 1150}; //ventor ave
-	    _rent3 = {24, 120, 360, 850, 1025, 1200}; //marvin gardens
+    	_rent1 = {22, 110, 330, 800, 975, 1150}; //atlantic ave | ventor ave
+	_rent2 = {24, 120, 360, 850, 1025, 1200}; //marvin gardens
 	    
 	// set buy prices
     	_buyPrice1 = 260; 
-    	_buyPrice2 = 260; 
-	    _buyPrice3 = 280; 
+    	_buyPrice2 = 280; 
 	    
 	//set house cost and mortgage value
     	_houseCost = 150;
     	_mortgageValue1 = (int) (.5 * _buyPrice1);
     	_mortgageValue2 = (int) (.5 * _buyPrice2);
-    	_mortgageValue3 = (int) (.5 * _buyPrice3);
 
     }
 }
