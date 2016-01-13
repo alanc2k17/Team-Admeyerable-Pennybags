94b35f95e9dc61f37d8f34adaa485bb807cf7a00
alanc2k17 <ychen15@stuy.edu>
2016-01-12 21:03:21 -0500

    Update RedProperty.java

--- Property_subclasses/NormalProperty_subclasses/RedProperty.java
+++ Property_subclasses/NormalProperty_subclasses/RedProperty.java
@@ -15,20 +15,17 @@ public class RedProperty extends NormalProperty{
     //set rent prices, buy prices, house costs, and mortgage values
     public void setInfo(){
 	// set rent prices, index corresponds w/ # of houses
-    	_rent1 = {18, 90, 250, 700, 875, 1050}; //kentucky ave
-    	_rent2 = {18, 90, 250, 700, 875, 1050}; //indiana ave
-	    _rent3 = {20, 100, 300, 750, 925, 1100}; //illinois ave
+    	_rent1 = {18, 90, 250, 700, 875, 1050}; //kentucky ave | indiana ave
+	_rent3 = {20, 100, 300, 750, 925, 1100}; //illinois ave
 	    
 	// set buy prices
     	_buyPrice1 = 220; 
-    	_buyPrice2 = 220; 
-	    _buyPrice3 = 240; 
+    	_buyPrice2 = 240; 
 	    
 	//set house cost and mortgage value
     	_houseCost = 150;
     	_mortgageValue1 = (int) (.5 * _buyPrice1);
     	_mortgageValue2 = (int) (.5 * _buyPrice2);
-    	_mortgageValue3 = (int) (.5 * _buyPrice3);
 
     }
 }
