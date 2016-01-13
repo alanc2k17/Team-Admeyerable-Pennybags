26022055f4e47ce35b211a3b0c67034b127f984e
Calvin Vuong <calvin@boxer.local>
2016-01-12 19:01:34 -0500

    completed BrownProperty.java

--- Property_subclasses/NormalProperty_subclasses/BrownProperty.java
+++ Property_subclasses/NormalProperty_subclasses/BrownProperty.java
@@ -4,6 +4,24 @@ public class BrownProperty extends NormalProperty{
     protected final int[] RENT1 = {2, 10, 30, 90, 160, 250}; //mediterranean
     protected final int[] RENT2 = {4, 20, 60, 180, 320, 450}; //baltic
 
+    // pre-set buy prices
+    protected final int BUYPRICE1 = 60; //mediterranean
+    protected final int BUYPRICE2 = 60; //baltic
     
+    //pre-set house cost and mortgage value
+    protected final int HOUSECOST = 50;
+    protected final int MORTGAGEVALUE1 = 30;
+    protected final int MORTGAGEVALUE2 = 30;
+
+    //default constructor
+    public BrownProperty(){
+	this();	
+    }
+
+    //overloaded constructor
+    public BrownProperty(String name, String initials, String coordinate, boolean expensive){
+	this(name, initials, coordinate, expensive);
+    }
+}
 
     
