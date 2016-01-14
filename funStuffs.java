e1679d3affb0e84fd8d0cc4619f33495f39e2747
alanc2k17 <ychen15@stuy.edu>
2016-01-13 20:29:59 -0500

    Create funStuffs.java

---
+++ funStuffs.java
@@ -0,0 +1,23 @@
+//pseudoloop for printing board
+//GIVEN: board[11][11]
+
+String currentRow = "";
+
+for (int i = 0; i < 11; i++) {
+    currentRow = "|";
+    for (int j = 0; j < 11; j++) {
+      if (board[i][j] == null) 
+        currentRow += "\t|";
+      else 
+        currentRow += <Property>._initial + "|"
+    }
+  }
+  
+if (board[<Player>._coords *NOT SURE HOW TO SPLIT YET*].isOwned() == false) {
+    System.out.println(<Property>)
+    System.out.println(*options for player*)
+    }
+else {
+    *PAY RENT*
+    }
+  
