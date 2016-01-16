8092e1e3816beaf4eaf2fac0818b3c719cf75a8e
alanc2k17 <ychen15@stuy.edu>
2016-01-16 11:56:26 -0500

    Update Monopoly.java

--- Monopoly.java
+++ Monopoly.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+
 public class Monopoly{
+    private int numPlayers;
+    private ArrayList<Player> playerList;
+    private Landable[][] board;
+    private int turns;
+    
+    public void setup() {
+    	// initialize board
+    	board = new Landable[11][11];
+    	// initialize properties
+    	
+    	// initialize players
+    }
+    	
+    public void turn() {
+    	// roll dice
+    	// move player
+    	// player actions
+    }
     
     public static void main(String[] args){
 	Monopoly game = new Monopoly();
