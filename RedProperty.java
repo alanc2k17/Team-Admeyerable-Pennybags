6ba4ee224b6a0dc357eefb49c23da1d9e3c73c04
alanc2k17 <ychen15@stuy.edu>
2016-01-16 20:18:08 -0500

    Update RedProperty.java

--- RedProperty.java
+++ RedProperty.java
@@ -7,8 +7,8 @@ public class RedProperty extends NormalProperty{
     }
 
     //overloaded constructor
-    public RedProperty(String name, String initials, String coordinate, boolean expensive){
-    	super(name, initials, coordinate, expensive);
+    public RedProperty(String name, String initials, boolean expensive){
+    	super(name, initials, expensive);
     	setInfo();
     }
 
