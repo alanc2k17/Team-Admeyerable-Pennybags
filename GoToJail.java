8a4dc227a98a5575c975e070c024135ded7a5f74
alanc2k17 <ychen15@stuy.edu>
2016-01-17 20:10:39 -0500

    Create GoToJail.java

---
+++ GoToJail.java
@@ -0,0 +1,15 @@
+public class GoToJail extends Landable {
+  //default constructor
+  public GoToJail() {
+    super();
+  }
+  
+  //overloaded constructor
+  public GoToJail(String name, String initials) {
+    super(name, initials);
+  }
+  
+  //overloaded toString
+  public String toString() {
+    return "Looks like you got caught by the po-po! Peace out, dawg.";
+  }
