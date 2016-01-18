2bb7f7c8b29dae698e247d478764580411e00241
alanc2k17 <ychen15@stuy.edu>
2016-01-17 20:12:33 -0500

    Create Jail.java

---
+++ Jail.java
@@ -0,0 +1,19 @@
+public class Jail extends Landable {
+  //default constructor
+  public Jail() {
+    super()
+  }
+  
+  //overloaded constructor
+  public Jail(String name, String ititials) {
+    super(name, initials);
+  }
+  
+  //overloaded toString
+  public String toString() {
+  String returnString;
+  returnString += "Oh man! You are currently in the gallows, What should you do?";
+  return returnString;
+  }
+  
+}
