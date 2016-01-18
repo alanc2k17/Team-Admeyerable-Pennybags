e97eb3a53ae70eee5c8695389fe44aaab3c28bef
alanc2k17 <ychen15@stuy.edu>
2016-01-17 20:07:53 -0500

    Create FreeParking.java

---
+++ FreeParking.java
@@ -0,0 +1,18 @@
+public class FreeParking extends Landable {
+  //default constructor
+  public FreeParking() {
+    super();
+  }
+  
+  //overloaded constructor
+  public FreeParking(String name, String initials) {
+    super(name, initials);
+  }
+  
+  public String toString() {
+    //teehee pointless easter egg! (1 of 4)
+    return "enjoy your free parking!";
+  }
+  
+}
+    
