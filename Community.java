a59598fa60638732ee85d780a72e1b7cd48ee971
alanc2k17 <ychen15@stuy.edu>
2016-01-20 18:02:10 -0500

    Update Community.java

--- Community.java
+++ Community.java
@@ -29,39 +29,59 @@ public class Community extends Landable {
   }
   
   //*reads* card and does what it says
-  public void execute(int n) {
+  public void execute(Player p, Landable[][] board) {
     if (n < 0 || n > 9)
       System.out.println("Oops! Something went wrong, we have a team of experienced monkeys on the case!");
     else {
       if (n = 0) {
         //bank error in your favor 
+       	System.out.println("Bank error in your favor! (Collect $200)");
+       	p.give(200);
       }
       if (n = 1) {
-        //do something
+        //doctors pees
+        System.out.println("Doctor's fees! (Pay $50)");
+        p.charge(50);
       }
       if (n = 2) {
-        //do something
+        //from sale of stock
+        System.out.println("From sale of stock you get $50!");
+        p.give(50);
       }
       if (n = 3) {
-        //do something
+        //grand opera night
+        System.out.println("Grand Opera Night! (Collect $50 from every player for opening night seats)");
+        //should landable extend monopoly?
       }
       if (n = 4) {
-        //something
+        //holiday fund matures
+        System.out.println("Holiday Fund matures! (Collect $100)");
+        p.give(100);
       }
       if (n = 5) {
-        //something
+        //income tax refund
+        System.out.println("Income Tax Refund! (Collect $20)");
+        p.give(20);
       }
       if (n = 6) {
-        //something
+        //birthday!
+        System.out.println("Feliz Cumpleanos! (Collect $10)");
+        p.give(10);
       }
       if (n = 7) {
-        //something
+        //life insurance matures
+        System.out.println("Life Insurance Matures! (Collect $100)");
+        p.give(100);
       }
       if (n = 8) {
-        //something
+        //pay hospital fees
+        System.out.println("Pay Hospital Fees! (Pay $100)");
+        p.charge(100);
       }
       if (n = 9) {
-        //something
+        //school tax
+        System.out.println("Time To Send The Kids To College! (Pay $150)");
+        p.charge(150);
       }
     }
   }
