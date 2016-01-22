6f6d68a7db71a83a8e310defc408e2514d64e62d
Calvin Vuong <calvin@boxer.local>
2016-01-21 23:43:38 -0500

    reworked board o fit on screen

--- Monopoly.java
+++ Monopoly.java
@@ -164,8 +164,8 @@ public class Monopoly{
     public void printBoard(){
 	clear();
 	for ( int row = 0; row < board.length; row++ ){
-	    // loop through each row 5 times, since each element consumes 5 lines
-	    for ( int count = 0; count < 5; count++ ){ 
+	    // loop through each row 3 times, since each element consumes 3 lines
+	    for ( int count = 0; count < 4; count++ ){ 
 		for ( int col = 0; col < board[row].length; col++ ){
 		    // if element is last in row, print newline
 		    // if not, print nothing
@@ -173,45 +173,38 @@ public class Monopoly{
 		    if ( col == board[row].length-1 )
 			newLine = "\n";
 
-		    // if element is null, print 5 empty spaces
+		    // if element is null, print 9 empty spaces
 		    if ( board[row][col] == null ){
 			if ( col == 0 )
 			    System.out.print("|"); //border
-			System.out.print("     " + newLine);
+			System.out.print("         " + newLine);
 		    }
 		    else{ //element is an instance of Landable
 			// if in first column or directly after a null...
 			if ( col == 0 || (board[row][col-1] == null) ) 
 			    System.out.print("|"); //border
 			
-			if ( count == 0 ) // on first count
-			    System.out.print("-----" + newLine); //border
+			if ( count == 0 ) /*(row == 0 || row == 10)*/ //
+			    System.out.print("---------" + newLine); //border
 
-			else if ( count == 1 ) // on second count, print initials
-			    System.out.print(" " + board[row][col].getInitials() + " |" + newLine);
-
-			else if ( count == 2){ // on third count, print houses if applicable 
-			    if ( board[row][col] instanceof NormalProperty ){
-				// typecast to access getHouses() method
-				NormalProperty element = (NormalProperty)(board[row][col]);
-				if ( element.getHouses() == 5 )
-				    System.out.print("  H |" + newLine); //print hotel
-				else{
-				    String printStr = "";
-				    // print proper num of houses
-				    for (int h = 0; h < element.getHouses(); h++)
-					printStr += "h";
-				    // print added spaces to keep padding
-				    while ( printStr.length() < 4 )
-					printStr += " ";
-				    System.out.print(printStr + "|" + newLine);
-				} // close if houses == 5
-			    } // close if normalproperty
-			    else // element is a railroad, utilty, or nonproperty
-				System.out.print("    |" + newLine); //print blank spaces (no houses)
-			} // close count 2
-			
-			else if (count == 3){ // on fourth count, print players on
+			if ( count == 1 ){ // on first count, print initials, owner, and houses
+			    String initials = board[row][col].getInitials() + ":";
+			    String symbol = " ";
+			    String houseString = "    ";
+			    if ( board[row][col] instanceof Property ){
+				Property prop = (Property)(board[row][col]); //typecast
+				if ( prop.getOwner() != null )
+				    symbol = prop.getOwner().getSymbol();
+				if ( prop instanceof NormalProperty ){
+				    int houses = ( (NormalProperty)(prop) ).getHouses();
+				    if ( houses != 0 )
+					houseString = "(" + houses + "h)";
+				}				
+			    }
+			    System.out.print(initials + symbol + houseString + "|" + newLine);
+			} //close count 1
+			    
+			if (count == 2){ // on second count, print players on square
 			    String printStr = ""; 
 			    // list of players on square
 			    ArrayList<Player> playersOn = board[row][col].getPlayersOn();
@@ -219,16 +212,16 @@ public class Monopoly{
 			    for ( int i = 0; i < playersOn.size(); i++ )
 				printStr += playersOn.get(i).getSymbol();
 			    //print added spaces to keep padding
-			    for ( int i = playersOn.size(); i < 4; i++ )
+			    for ( int i = playersOn.size(); i < 8; i++ )
 				printStr += " ";
 			    System.out.print(printStr + "|" + newLine);
-			} // close count 3
+			} // close count 2
 			
-			else if ( count == 4 ) // on fifth count 
-			    System.out.print("-----" + newLine); //border
+			if ( count == 3 ) // on third count 
+			    System.out.print("---------" + newLine); //border
 		    } // close else instance of Landable
 		} // close loop through row 
-	    } // close loop 5 times
+	    } // close loop 3 times
 	} // close top level loop
     } // close method
 			    
@@ -347,7 +340,7 @@ public class Monopoly{
 		}
 		playerList.remove(p); // you lose!
 	    */
-	    }
+	}
 	else
 	    p.setJailTurns(p.getJailTurns() + 1);
     }
